d2623a40182eafa46ccd2b29c78d6290c45e8cbc
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2025-02-20 10:36:28 +0000

    Add ActivitiesByRegionMap component: implement dynamic regional activity visualization with loading state

--- components/survey_data/overview/ActivityByRegion.tsx
+++ components/survey_data/overview/ActivityByRegion.tsx
@@ -1,10 +1,25 @@
+"use client";
+
 import React from 'react'
+import dynamic from 'next/dynamic'
 
 export default function ActivityByRegion() {
+  const LandingPageMapComponent = React.useMemo(() => {
+    return dynamic(
+      () => import('../../chart_and_graphics/ActivitiesByRegionMap'),
+      {
+        loading: () => <p>Loading Map...</p>,
+        ssr: false
+      }
+    );
+  }, []);
   return (
       <section className='mb-8' id='activity-by-region'>
-          <h1 className='text-3xl text-navy-blue font-medium'>Activity By Region</h1>
-          <div className='h-[90vh] bg-white rounded-2xl'></div>
+      <h1 className='text-3xl text-navy-blue font-medium'>Activity By Region</h1>
+
+      <div className='h-[100dvh] bg-white rounded-2xl overflow-hidden'>
+        <LandingPageMapComponent />
+      </div>
     </section>
   )
 }
