dbf6b4aa07369bbf69d745b5130bd4b38ecb7a97
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2025-02-20 17:24:44 +0000

    Replace ActivitiesByDiseaseArea component with VerticalBarchart in DiseasesArea for improved data visualization

--- components/survey_data/overview/DiseasesArea.tsx
+++ components/survey_data/overview/DiseasesArea.tsx
@@ -1,12 +1,13 @@
-import ActivitiesByDieseaseArea from '@/components/chart_and_graphics/ActivitiesByDiseaseArea'
 import React from 'react'
+import VerticalBarchart from '@/components/chart_and_graphics/VerticalBarchart'
+import { ACTIVITIES_BY_REGION_DATA } from '@/constant'
 
 export default function DiseasesArea() {
   return (
     <section className='mb-8' id='diseases'>
       <h1 className='text-3xl text-navy-blue font-medium'>Activity by Diseases Area</h1>
       <div className='min-h-[90vh] bg-white rounded-2xl'>
-        <ActivitiesByDieseaseArea />
+        <VerticalBarchart data={ACTIVITIES_BY_REGION_DATA} name='Activities by Diseases Area' />
       </div>
     </section>
   )
