c5a36a5351f119610a89780897d6d9e2bb75bd15
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2025-02-21 10:32:10 +0000

    Refactor section IDs for FundingSource, NumberOfProjects, and StakeholderDetails components for improved clarity

--- components/survey_data/background/NumberOfProjects.tsx
+++ components/survey_data/background/NumberOfProjects.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function NumberOfProjects() {
   return (
-      <section className='mb-8' id='number-of-projects'>
+    <section className='mb-8' id='projects'>
           <h1 className='text-3xl text-navy-blue font-medium'>Number of Projects</h1>
           <div className='h-[90vh] bg-white rounded-2xl'></div>
       </section>
--- components/survey_data/background/StakeholderDetails.tsx
+++ components/survey_data/background/StakeholderDetails.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function StakeholderDetails() {
   return (
-      <section className='mb-8' id='stakeholder-details'>
+    <section className='mb-8' id='stakeholder-info'>
           <h1 className='text-3xl text-navy-blue font-medium'>Stakeholder Details</h1>
           <div className='h-[90vh] bg-white rounded-2xl'></div>
       </section>
