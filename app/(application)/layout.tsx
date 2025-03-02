a91efa10f1de14839eef1791c787f3c8c22fd61e
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2025-03-02 17:12:01 +0000

    Implement authentication redirection in RootLayout for Admin and User roles

--- app/(application)/layout.tsx
+++ app/(application)/layout.tsx
@@ -1,17 +1,29 @@
 import type { Metadata } from "next";
 import Header from '@/components/shared/Header';
 import Footer from '@/components/shared/Footer';
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: "NCD Navigator",
   description: "A tool to help you navigate the NCD landscape",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const session = await auth();
+  const role = session?.user?.role;
+
+  if (session && role === 'Admin') {
+    return redirect('/admin/dashboard');
+  } else if (session && role === 'User') {
+    return redirect('/dashboard');
+  } 
+
   return (
     <main>
       <Header />
