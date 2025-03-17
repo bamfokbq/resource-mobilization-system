4a8c675f160e76199be3b41ef089ac8f3999e687
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2025-03-17 23:07:18 +0000

    Add HealthStats component to the main page for enhanced health data visualization

--- app/(application)/page.tsx
+++ app/(application)/page.tsx
@@ -1,3 +1,4 @@
+import HealthStats from '@/components/chart_and_graphics/HealthStats';
 import NcdStats from '@/components/chart_and_graphics/NcdStats';
 import Assistance from '@/components/features/Assistance';
 import Contributions from '@/components/features/Contributions';
@@ -12,6 +13,7 @@ export default function Home() {
       <Features />
       <Contributions />
       <NcdStats />
+      <HealthStats />
       <ResourcesAction />
       <Assistance />
     </main>
