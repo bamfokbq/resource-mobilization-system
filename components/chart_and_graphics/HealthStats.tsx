3618ad6d53da2b1be053e917a8fbe8428862ab2e
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2025-03-18 09:45:48 +0000

    Add contact information section to HealthStats component

--- components/chart_and_graphics/HealthStats.tsx
+++ components/chart_and_graphics/HealthStats.tsx
@@ -560,6 +560,31 @@ export default function HealthStats() {
             </div>
           </div>
         </div>
+        {/* Contact Information */}
+        <div className="mt-6 bg-gray-50 p-4 rounded-lg border border-gray-200">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
+              <span className="text-sm font-medium text-gray-700">Ghana Health Service ICT Department</span>
+            </div>
+            <div className="flex space-x-4">
+              <div className="flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                </svg>
+                <a href="mailto:info@ghsict.gov" className="text-sm text-blue-600 hover:underline">info@ghsict.gov</a>
+              </div>
+              <div className="flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                </svg>
+                <a href="tel:+233243127089" className="text-sm text-blue-600 hover:underline">+233 24 312 7089</a>
+              </div>
+            </div>
+          </div>
+        </div>
         {/* Add export button */}
         <div className="mt-6 flex justify-end">
           <Button
