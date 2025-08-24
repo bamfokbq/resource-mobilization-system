a7affdaa681cb044ddc96b224267f9d663711232
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2025-08-24 23:12:47 +0100

    feat(Resources): implement ResourcesPage with grid, pagination, and search/filter components

--- app/(application)/resources/page.tsx
+++ app/(application)/resources/page.tsx
@@ -1,7 +1,14 @@
+import ResourcesGrid from '@/components/resources/ResourcesGrid'
+import ResourcesPagination from '@/components/resources/ResourcesPagination'
+import ResourcesSearchAndFilter from '@/components/resources/ResourcesSearchAndFilter'
 import React from 'react'
 
 export default function ResourcesPage() {
   return (
-    <section>Resources Page</section>
+    <section>
+      <ResourcesSearchAndFilter />
+      <ResourcesGrid />
+      <ResourcesPagination />
+    </section>
   )
 }
---
+++ components/resources/ResourcesSearchAndFilter.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ResourcesSearchAndFilter() {
+  return (
+    <section>ResourcesSearchAndFilter</section>
+  )
+}
