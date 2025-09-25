6dc0b534e659ac54c8031c7fed2d205f873e71ad
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2025-09-25 06:28:12 +0100

    refactor: adjust layout in LandingHero component for better responsiveness

--- components/features/LandingHero.tsx
+++ components/features/LandingHero.tsx
@@ -5,7 +5,7 @@ import LandingHeroArt from './LandingHeroArt'
 export default function LandingHero() {
     return (
         <section className='min-h-[85dvh] flex justify-center'>
-            <div className='flex md:flex-col'>
+            <div className='flex flex-col md:flex-row w-full'>
                 <LandingHeroContent />
                 <LandingHeroArt />
             </div>
